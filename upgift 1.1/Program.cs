faba2413da6a3f67cbf5430832ae988f769aa822
taal04003 <taal04003@E-PW0234KH>
2023-09-06 10:41:07 +0200

    Add project files.

---
+++ upgift 1.1/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Du är nästan klar med kapitel 1.");
+Console.WriteLine("Du har lärt dig skriva ut tecken som &quot; och \\.");
